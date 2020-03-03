0d485f5d77741e7a080b681a47a9bba41aa9ee28
namekinggamerboy <53903319+namekinggamerboy@users.noreply.github.com>
2020-03-03 09:10:51 +0530

    Create allemoji.js

---
+++ commands/allemoji.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => { 
+let emojis;
+        if (message.guild.emojis.size === 0) emojis = 'There are no emojis on this server.';
+        let emojisemb = new Discord.RichEmbed()
+        .setTitle(`${message.guild.name} Emojis`)
+.setColor("0099ff")  .setDescription(`${message.guild.emojis.map(e => e).join(' ')}`)
+        message.channel.send(emojisemb);
+
+}
